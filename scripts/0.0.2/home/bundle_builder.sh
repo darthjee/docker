da3aed90f4e5e8c8c495ee953c6f74f120136ed0
darthjee <darthjee@gmail.com>
2019-06-10 21:25:41 +0200

    Copy all bundle files

--- scripts/0.0.2/home/bundle_builder.sh
+++ scripts/0.0.2/home/bundle_builder.sh
@@ -4,6 +4,8 @@ cp /usr/local/bundle $HOME_DIR/bundle_cache -R
 mkdir -p $HOME_DIR/bundle/gems/
 mkdir -p $HOME_DIR/bundle/cache/
 mkdir -p $HOME_DIR/bundle/specifications/
+mkdir -p $HOME_DIR/bundle/bin/
+mkdir -p $HOME_DIR/bundle/extensions/
 
 bundle install --clean
 
@@ -13,5 +15,19 @@ for PATH in /usr/local/bundle/gems/*; do
     cp $PATH $HOME_DIR/bundle/gems/$GEM -R
     cp /usr/local/bundle/cache/$GEM.gem $HOME_DIR/bundle/cache -R
     cp /usr/local/bundle/specifications/$GEM.gemspec $HOME_DIR/bundle/specifications -R
+  fi
+done
+
+for PATH in /usr/local/bundle/bin/*; do
+  BIN=${PATH##/usr/local/bundle/bin/}
+  if [ ! -x $HOME_DIR/bundle_cache/bin/$BIN ]; then
+    cp $PATH $HOME_DIR/bundle/bin/$BIN -R
+  fi
+done
+
+for PATH in /usr/local/bundle/extensions/*; do
+  EXT=${PATH##/usr/local/bundle/extensions/}
+  if [ ! -x $HOME_DIR/bundle_cache/extensions/$EXT ]; then
+    cp $PATH $HOME_DIR/bundle/extensions/$EXT -R
   fi
 done
