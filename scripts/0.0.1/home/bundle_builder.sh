94be6fe94dd9cb22057f4d8303d7a4bdb9909e4a
Favini <darthjee@gmail.com>
2019-06-10 19:01:10 +0200

    Scripts (#5)

---
+++ scripts/0.0.1/home/bundle_builder.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cp /usr/local/bundle $HOME_DIR/bundle_cache -R
+mkdir -p $HOME_DIR/bundle/gems/
+mkdir -p $HOME_DIR/bundle/cache/
+mkdir -p $HOME_DIR/bundle/specifications/
+
+bundle install --clean
+
+for PATH in /usr/local/bundle/gems/*; do
+  GEM=${PATH##/usr/local/bundle/gems/}
+  if [ ! -x $HOME_DIR/bundle_cache/gems/$GEM ]; then
+    cp $PATH $HOME_DIR/bundle/gems/$GEM -R
+    cp /usr/local/bundle/cache/$GEM.gem $HOME_DIR/bundle/cache -R
+    cp /usr/local/bundle/specifications/$GEM.gemspec $HOME_DIR/bundle/specifications -R
+  fi
+done
