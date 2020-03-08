298773241dd10ffb42df8fa5101b3453cf165ed2
darthjee <darthjee@gmail.com>
2020-03-08 21:46:44 +0100

    Add scripts:0.1.7

---
+++ scripts/0.1.7/home/builder/bower_builder.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ARGS=$(echo $* | xargs)
+
+mkdir -p $HOME_DIR/.cache/bower 
+cp $HOME_DIR/.cache/bower $HOME_DIR/bower_cache -R
+mkdir -p $HOME_DIR/bower/registry/registry.bower.io/lookup/
+mkdir -p $HOME_DIR/bower/packages/
+
+bower install $ARGS
+
+for PATH in $HOME_DIR/.cache/bower/registry/registry.bower.io/lookup/*; do
+  SOURCE=$HOME_DIR/.cache/bower/registry/registry.bower.io/lookup/
+  DEST=$HOME_DIR/bower/registry/registry.bower.io/lookup/
+  PACKAGE=${PATH##$SOURCE}
+
+  if [ ! -x $DEST/$PACKAGE ]; then
+    cp $PATH $DEST -R
+  fi
+done
+
+for PATH in $HOME_DIR/.cache/bower/packages/*/*; do
+  SOURCE=$HOME_DIR/.cache/bower/packages/
+  PAKAGE_FULL_PATH=${PATH##$SOURCE}
+  DEST=$HOME_DIR/bower/packages/
+
+  PACKAGE=${PAKAGE_FULL_PATH%%/*}
+  VERSION=${PAKAGE_FULL_PATH##*/}
+
+  if [ ! -x $DEST/$PACKAGE/$VERSION ]; then
+    mkdir -p $DEST/$PACKAGE
+    cp $PATH $DEST/$PACKAGE -R
+  fi
+done
