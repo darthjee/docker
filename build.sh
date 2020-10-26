76fe3e96a3775159a6354ea5766e609960389744
darthjee <darthjee@gmail.com>
2020-10-26 21:09:27 +0100

    Infer version

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+function current_version() {
+  IMAGE=$1
+  echo $(cat version | grep "^"$IMAGE"=" | sed -e "s/"$IMAGE"=//g")
+}
+
 function version() {
   IMAGE=$1
   VERSION=$2
 
   if [ ! $VERSION ]; then
-    VERSION=$(cat version | grep "^"$IMAGE"=" | sed -e "s/"$IMAGE"=//g")
+    VERSION=$(current_version $IMAGE)
+    LAST=$(echo $VERSION | sed "s/.*\.//g")
+    LAST=$[$LAST+1]
+    VERSION=$(echo $VERSION | sed -e "s/\.[^.]*$/.$LAST/g")
   fi
 
   echo $VERSION
@@ -16,7 +24,9 @@ function build() {
 
   if [ $IMAGE ]; then
     VERSION=$(version $IMAGE $3)
-    echo $VERSION
+    CURRENT_VERSION=$(current_version $IMAGE)
+
+    echo cp -R $IMAGE/$CURRENT_VERSION $IMAGE/$VERSION
   else
     help
   fi
