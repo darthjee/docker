a776a90ddde79008f6a506a56378d6b92765e89e
darthjee <darthjee@gmail.com>
2020-10-26 22:35:28 +0100

    Add copy_deps

--- build.sh
+++ build.sh
@@ -53,8 +53,30 @@ function build() {
   fi
 }
 
-function copy_gems() {
-  1
+function copy_home() {
+  IMAGE_NAME=$1
+  VERSION=$2
+
+  if [ -d $IMAGE_NAME ]; then
+    cp -R $IMAGE/$VERSION/home/* $IMAGE_NAME/$VERSION/home/
+  else
+    echo skipping $IMAGE_NAME
+  fi
+}
+
+function copy_deps() {
+  IMAGE=$2
+
+  if [ $IMAGE ]; then
+    VERSION=$(current_version $IMAGE)
+
+    for MOD in circleci_ production_; do
+      copy_home $MOD$IMAGE $VERSION
+    done
+
+  else
+    help
+  fi
 }
 
 function help() {
@@ -70,7 +92,7 @@ case $ACTION in
     build $*
     ;;
   "copy_deps")
-    copy_gems
+    copy_deps $*
     ;;
   *)
     help
