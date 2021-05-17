73cb1e3239e3fd48f90f6ccbfcf5b6eabaea1cd4
darthjee <darthjee@gmail.com>
2021-05-17 18:48:50 +0200

    Correct script

--- heroku/0.0.1/script/lib/install.sh
+++ heroku/0.0.1/script/lib/install.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+CMD=$0
+CMD_FOLDER=$(echo $CMD | sed -e "s/\\/lib\\/[^\\/]*$//g")
 SCRIPT_FOLDER=.dt-heroku
 SCRIPT_PATH=$HOME/$SCRIPT_FOLDER
 ACTION=$1
 BASH_FILE=$HOME/.bashrc
 
 if [ ! $ACTION ]; then
+  cp -R $CMD_FOLDER/ $SCRIPT_PATH/
+
   if (! grep $SCRIPT_FOLDER $BASH_FILE > /dev/null); then
     echo "eval \$(\$HOME/$SCRIPT_FOLDER/heroku install -)" >> $BASH_FILE
     eval $($SCRIPT_PATH/heroku install -)
   fi
+
   exit 0
 fi
 
