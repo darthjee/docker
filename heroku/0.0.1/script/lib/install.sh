849de72ff4db250b05f7d61026533712ce35424c
darthjee <darthjee@gmail.com>
2021-05-17 14:42:04 +0200

    Update script

--- heroku/0.0.1/script/lib/install.sh
+++ heroku/0.0.1/script/lib/install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-SCRIPT_FOLDER=$HOME/.dt-heroku
+SCRIPT_FOLDER=.dt-heroku
+SCRIPT_PATH=$HOME/$SCRIPT_FOLDER
 ACTION=$1
 BASH_FILE=$HOME/.bashrc
 
 if [ ! $ACTION ]; then
   if (! grep $SCRIPT_FOLDER $BASH_FILE > /dev/null); then
     echo "eval \$(\$HOME/$SCRIPT_FOLDER/heroku install -)" >> $BASH_FILE
+    eval $($SCRIPT_PATH/heroku install -)
   fi
 fi
