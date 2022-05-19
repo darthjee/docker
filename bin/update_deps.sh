9dd19f1be38846b5f918bd3ccd7a7c089a66af65
darthjee <darthjee@gmail.com>
2022-05-19 14:08:45 +0200

    Parse gemfile

--- bin/update_deps.sh
+++ bin/update_deps.sh
@@ -1,3 +1,10 @@
+function update_gemfile() {
+  ORIGIN=$1/home/Gemfile
+  DEST=$2/home/Gemfile
+
+  cat $ORIGIN | grep gem | sed -e "s/.*'\\(.*\\)'.*'\(.*\)'/\\1:\\2/g"
+}
+
 function update_deps() {
   IMAGE=$2
   SOURCE=$3
@@ -5,6 +12,7 @@ function update_deps() {
   if [ $IMAGE ] && [ $SOURCE ]; then
     VERSION=$(current_version $IMAGE)
     SOURCE_VERSION=$(current_version $SOURCE)
+    update_gemfile $SOURCE/$SOURCE_VERSION $IMAGE/$VERSION
   else
     help
   fi
