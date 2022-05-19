344d4e2828c1ef43dd25a22aa30b410428fb0372
darthjee <darthjee@gmail.com>
2022-05-19 14:00:44 +0200

    Add update deps

--- bin/script.sh
+++ bin/script.sh
@@ -5,12 +5,14 @@ source 'bin/release.sh'
 source 'bin/test.sh'
 source 'bin/init.sh'
 source 'bin/copy_deps.sh'
+source 'bin/update_deps.sh'
 source 'bin/pre_build.sh'
 
 function help() {
     echo Usage:
     echo "$0 init <image> [new_version] # inits a new version"
     echo "$0 copy_deps <image> # copy dependencies files"
+    echo "$0 update_deps <image> <source> # copy dependencies from one image to another"
     echo "$0 build <images> # build set of images"
     echo "$0 release <images> # release set of images"
     echo "$0 test <images> # test an image"
@@ -25,6 +27,9 @@ case $ACTION in
     ;;
   "copy_deps")
     copy_deps $*
+    ;;
+  "update_deps")
+    update_deps $*
     ;;
   "build")
     build $*
