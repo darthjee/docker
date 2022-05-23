7987e8255d5d1e9ab768252215d08fbdfb2644b5
Fernando Favini <darthjee@gmail.com>
2022-05-23 18:50:17 +0200

    Ruby270 (#55)

---
+++ bin/update_deps.sh
@@ -0,0 +1,25 @@
+function update_gemfile() {
+  ORIGIN=$1/home/Gemfile
+  DEST=$2/home/Gemfile
+
+  for LINE in $(cat $ORIGIN | grep "^\\s*gem" | sed -e "s/.*'\\(.*\\)'.*'\(.*\)'.*/\\1:\\2/g" ); do
+    GEM=$(echo $LINE | sed -e "s/\\(.*\\):\(.*\)/\\1/g")
+    VERSION=$(echo $LINE | sed -e "s/\\(.*\\):\(.*\)/\\2/g")
+    cat $DEST | sed "s/\\('$GEM'.*\\)'.*'/\\1'$VERSION'/g" > tmp.txt
+    mv tmp.txt $DEST
+  done
+}
+
+function update_deps() {
+  IMAGE=$2
+  SOURCE=$3
+
+  if [ $IMAGE ] && [ $SOURCE ]; then
+    VERSION=$(current_version $IMAGE)
+    SOURCE_VERSION=$(current_version $SOURCE)
+    update_gemfile $SOURCE/$SOURCE_VERSION $IMAGE/$VERSION
+  else
+    help
+  fi
+
+}
