cd74e7f3a1227f848ef0b9df3df796d59f53dc0a
darthjee <darthjee@gmail.com>
2022-08-09 06:00:08 -0300

    Extract missing gems method

--- scripts/0.3.0/home/sbin/check_gems.sh
+++ scripts/0.3.0/home/sbin/check_gems.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+function missing_gems() {
+  bundle check 2>&1 | grep " (" | sed -e "s/ *\* *\(.*\) (\(.*\))/\1:\2/g"
+}
+
 function check_bundle() {
-  LIST=$(bundle check 2>&1 | grep " (" | sed -e "s/ *\* *\(.*\) (\(.*\))/\1:\2/g")
+  LIST=$(missing_gems)
 
   if [[ $LIST ]]; then
     exit 1
