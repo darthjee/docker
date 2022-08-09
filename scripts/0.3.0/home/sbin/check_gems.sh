a398db867b7fa1dda34416ea834c102d2f686a4a
darthjee <darthjee@gmail.com>
2022-08-09 07:41:02 -0300

    Upgrade gems

--- scripts/0.3.0/home/sbin/check_gems.sh
+++ scripts/0.3.0/home/sbin/check_gems.sh
@@ -14,14 +14,43 @@ function check_bundle() {
   fi
 }
 
+function upgrade_all() {
+  LIST=$(missing_gems)
+
+  for MATCH in $LIST; do
+    NAME=${MATCH%%:*}
+    VERSION=${MATCH##*:}
+    NEW_VERSION=$(latest_version $NAME)
+
+    if [[ $NEW_VERSION ]]; then
+      sed -e "s/gem ['\"]$NAME['\"] *$/gem '$NAME', '$NEW_VERSION'/g" -i Gemfile
+    else
+      echo "$NAME -- $VERSION -- |$NEW_VERSION|"
+    fi
+  done
+}
+
+function latest_version() {
+  NAME=$1
+  LINE=$(gem list | grep "^$NAME ")
+
+  if [[ $LINE ]]; then
+    echo $LINE | sed -e "s/.* (//g" | sed -e "s/) *//g" | sed -e "s/,//g" | sed -e "s/ /\\n/g" | head -n 1
+  fi
+}
+
 ACTION=$1
 
 case $ACTION in
   "check")
     check_bundle
     ;;
+  "upgrade")
+    upgrade_all
+    ;;
   *)
     echo "Usage:"
     echo "$0 check # checks if gems have been installed"
+    echo "$0 upgrade gems from gemfile when they are outdated # checks if gems have been installed"
     ;;
 esac
