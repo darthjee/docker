f5878321e6e1ac9207ba2cc39b9526a1cde312b3
darthjee <darthjee@gmail.com>
2024-06-06 18:09:39 -0300

    Init yarn builder

--- scripts/0.4.1/home/builder/yarn_builder.sh
+++ scripts/0.4.1/home/builder/yarn_builder.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-ARGS=$(echo $* | xargs)
+if [ ! $MODULES_FOLDER ]; then
+  MODULES_FOLDER=/usr/lib/node_modules
+fi
+
+cp $MODULES_FOLDER $HOME_DIR/modules_cache -R
+mkdir -p $HOME_DIR/node_modules
 
 SED='/[dD]ependencies": {/,/}/ {s/    "\([^"]*\)": *"\(.*\)",\?/\1@\2/gp}'
-
 echo $SED > /tmp/package_sed.sed
-
-#sed -f /tmp/package_sed.sed package.json -n > out.txt
 npm install -g $(sed -f /tmp/package_sed.sed package.json -n)
+
+for FILE_PATH in $MODULES_FOLDER/*; do
+  PACKAGE=${FILE_PATH##$MODULES_FOLDER/}
+  if [ ! -x $HOME_DIR/node_modules/$PACKAGE ]; then
+    cp $FILE_PATH $HOME_DIR/bundle/gems/$GEM -R
+    cp $MODULES_FOLDER/cache/$GEM.gem $HOME_DIR/bundle/cache -R
+    cp $MODULES_FOLDER/specifications/$GEM.gemspec $HOME_DIR/bundle/specifications -R
+  fi
+done
