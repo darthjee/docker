6ae85689a4031eeca90936033ffbdbaf9a28ceb7
darthjee <darthjee@gmail.com>
2024-06-07 21:48:15 -0300

    Copy installed files

--- scripts/0.4.1/home/builder/yarn_builder.sh
+++ scripts/0.4.1/home/builder/yarn_builder.sh
@@ -18,16 +18,18 @@ function installPackages() {
   npm install -g $(sed -f /tmp/package_sed.sed package.json -n)
 }
 
+function copyNewInstalled() {
+  for FILE_PATH in $MODULES_FOLDER/*; do
+    PACKAGE=${FILE_PATH##$MODULES_FOLDER/}
+    PACKAGE_CACHE_PATH=$CACHE_DIR/$PACKAGE
+    NEW_PACKAGE_PATH=$NEW_MODULES_DIR/$PACKAGE
+
+    if [ ! -x $PACKAGE_CACHE_PATH ]; then
+       cp -R $FILE_PATH $NEW_PACKAGE_PATH
+    fi
+  done
+}
+
 createFolders
 installPackages
-
-for FILE_PATH in $MODULES_FOLDER/*; do
-  PACKAGE=${FILE_PATH##$MODULES_FOLDER/}
-  echo package $PACKAGE
-  echo PATH $HOME_DIR/modules_cache/$PACKAGE
-  if [ ! -x $HOME_DIR/modules_cache/$PACKAGE ]; then
-    echo NEW PACKAGE $PACKAGE
-  else
-    echo OLD PACKAGE $PACKAGE
-  fi
-done
+copyNewInstalled
