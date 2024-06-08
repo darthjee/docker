0467dd10cee8d5c88f913f45a5508c0074972a55
darthjee <darthjee@gmail.com>
2024-06-07 21:40:17 -0300

    Extract to functions

--- scripts/0.4.1/home/builder/yarn_builder.sh
+++ scripts/0.4.1/home/builder/yarn_builder.sh
@@ -4,12 +4,22 @@ if [ ! $MODULES_FOLDER ]; then
   MODULES_FOLDER=/usr/lib/node_modules
 fi
 
-cp $MODULES_FOLDER $HOME_DIR/modules_cache -R
-mkdir -p $HOME_DIR/node_modules
+CACHE_DIR=$HOME_DIR/modules_cache
+NEW_MODULES_DIR=$HOME_DIR/node_modules
 
-SED='/[dD]ependencies": {/,/}/ {s/    "\([^"]*\)": *"\(.*\)",\?/\1@\2/gp}'
-echo $SED > /tmp/package_sed.sed
-npm install -g $(sed -f /tmp/package_sed.sed package.json -n)
+function createFolders() {
+  cp $MODULES_FOLDER $CACHE_DIR -R
+  mkdir -p $NEW_MODULES_DIR
+}
+
+function installPackages() {
+  SED='/[dD]ependencies": {/,/}/ {s/    "\([^"]*\)": *"\(.*\)",\?/\1@\2/gp}'
+  echo $SED > /tmp/package_sed.sed
+  npm install -g $(sed -f /tmp/package_sed.sed package.json -n)
+}
+
+createFolders
+installPackages
 
 for FILE_PATH in $MODULES_FOLDER/*; do
   PACKAGE=${FILE_PATH##$MODULES_FOLDER/}
@@ -17,5 +27,7 @@ for FILE_PATH in $MODULES_FOLDER/*; do
   echo PATH $HOME_DIR/modules_cache/$PACKAGE
   if [ ! -x $HOME_DIR/modules_cache/$PACKAGE ]; then
     echo NEW PACKAGE $PACKAGE
+  else
+    echo OLD PACKAGE $PACKAGE
   fi
 done
