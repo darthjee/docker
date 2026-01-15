f1459e61f2759c594ef6d088eafc33de92e599a6
darthjee <darthjee@gmail.com>
2026-01-15 17:58:40 -0300

    Style fix

--- scripts/0.5.2/home/sbin/deploy_frontend.sh
+++ scripts/0.5.2/home/sbin/deploy_frontend.sh
@@ -8,26 +8,26 @@ function run_build() {
 }
 
 function run_generate_ssh_key_file() {
-    echo "$SSH_PRIVATE_KEY" | sed -e "s/\\\n/\n/g" > $SSH_KEY_FILE_PATH
-    chmod 600 $SSH_KEY_FILE_PATH
+    echo "$SSH_PRIVATE_KEY" | sed -e "s/\\\n/\n/g" > "$SSH_KEY_FILE_PATH"
+    chmod 600 "$SSH_KEY_FILE_PATH"
 }
 
 function run_upload() {
     SSH_COMMAND="ssh -i $SSH_KEY_FILE_PATH -p $SSH_PORT -o StrictHostKeyChecking=no"
-    rsync -avz -e "$SSH_COMMAND" $SOURCE $SSH_USER@$SSH_HOST:$SSH_REMOTE_TEMP_DIR
+    rsync -avz -e "$SSH_COMMAND" "$SOURCE" "$SSH_USER"@"$SSH_HOST":"$SSH_REMOTE_TEMP_DIR"
 }
 
 function run_release() {
     SSH_COMMAND="ssh -i $SSH_KEY_FILE_PATH -p $SSH_PORT -o StrictHostKeyChecking=no"
-    OLD_SSH_REMOTE_DIR=$SSH_REMOTE_DIR"_old_$(date +%s)"
+    OLD_SSH_REMOTE_DIR="$SSH_REMOTE_DIR""_old_$(date +%s)"
 
     COMMANDS="rm -rf $OLD_SSH_REMOTE_DIR && mv $SSH_REMOTE_DIR $OLD_SSH_REMOTE_DIR && mv $SSH_REMOTE_TEMP_DIR $SSH_REMOTE_DIR && rm -rf $OLD_SSH_REMOTE_DIR"
-    $SSH_COMMAND $SSH_USER@$SSH_HOST "$COMMANDS"
+    "$SSH_COMMAND" "$SSH_USER"@"$SSH_HOST" "$COMMANDS"
 }
 
 ACTION=$1
 
-case $ACTION in
+case "$ACTION" in
   "build")
     run_build
     ;;
