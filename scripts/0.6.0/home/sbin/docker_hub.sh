674af4686bbc9cbd0814f1256619e90174ebc9c1
darthjee <darthjee@gmail.com>
2026-01-19 12:57:42 -0300

    Login on both places

--- scripts/0.6.0/home/sbin/docker_hub.sh
+++ scripts/0.6.0/home/sbin/docker_hub.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+function fetchDockerHubToken() {
+    curl -s https://hub.docker.com/v2/users/login/ \
+        -H "Content-Type: application/json" \
+        -d '{"username":"'"$DOCKER_HUB_USERNAME"'","password":"'"$DOCKER_HUB_PASSWORD"'"}' \
+    | jq -r .token
+}
+
 function run_login() {
     echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
+    export DOCKER_HUB_TOKEN=$(fetchDockerHubToken)
 }
 
 function run_push() {
     docker push $*
 }
+
+
 
 ACTION="$1"
 
@@ -17,6 +27,6 @@ case "$ACTION" in
     run_login
     ;;
   "push")
-    run_push
+    run_push $*
     ;;
 esac
