c6ab5856adad6e1f6c7195d179f7ffe4f7efdd1e
Fernando Favini <darthjee@gmail.com>
2026-03-11 20:28:40 -0300

    New Scritps version (#112)

---
+++ scripts/0.7.0/home/builder/yarn_builder.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+ARGS=("$@")
+
+if [ ! "$APP_DIR" ]; then
+  APP_DIR="$HOME_DIR"/app
+fi
+
+GLOBAL_CACHE_PATH=/usr/local/share/.cache/yarn/v6
+USER_CACHE_PATH="$HOME_DIR"/yarn/cached
+NEW_PACKAGES_PATH="$HOME_DIR"/yarn/new
+
+function createFolders() {
+  mkdir -p "$GLOBAL_CACHE_PATH"
+  mkdir -p "$USER_CACHE_PATH"
+  mkdir -p "$NEW_PACKAGES_PATH"
+}
+
+function installPackages() {
+  cd "$APP_DIR"
+  yarn install "${ARGS[@]}"
+}
+
+function copyNewPackages() {
+  for PACKAGE_PATH in "$GLOBAL_CACHE_PATH"/*; do
+    PACKAGE_NAME=${PACKAGE_PATH##$GLOBAL_CACHE_PATH/}
+    
+    if [ ! -d "$USER_CACHE_PATH/$PACKAGE_NAME" ]; then
+      cp -R "$PACKAGE_PATH" "$NEW_PACKAGES_PATH"/
+    fi
+  done
+}
+
+function createCache() {
+  cp -R "$GLOBAL_CACHE_PATH"/* "$USER_CACHE_PATH"/
+}
+
+createFolders
+createCache
+installPackages
+copyNewPackages
